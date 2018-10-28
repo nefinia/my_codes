e72cbf1d7cb1613c734f9a5a2e0682aa45d198d2
Sofia G. Gallego <nefinia@gmail.com>
2018-10-29 00:27:34 +0200

    Minor changes
    1) flip y-axis to avoid flipping image in the vertical direction
    2) 2-dimensional images are now also clipped
    3) Changed pyfits to astropy

--- FitShow.py
+++ FitShow.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
+
 __author__ = 'nefinia'
 import numpy as np
-from pyfits import getdata
+import astropy.io.fits as fits
 from sys import argv
+
 
 def rdarg(argv, key, type=None, default=None, listtype=int):
 	if len(argv) > 1:
@@ -31,7 +33,7 @@ cube = rdarg(argv, 'name', str, '')
 std = rdarg(argv, 'std', float, None)
 bin = rdarg(argv, 'bin', int, 1)
 dim = rdarg(argv, 'dim', int, 0)
-colors = rdarg(argv, 'color', bool, False)
+colors = rdarg(argv, 'color', bool, True)
 xmin = rdarg(argv, 'xmin', int, 0)
 xmax = rdarg(argv, 'xmax', int, None)
 ymin = rdarg(argv, 'ymin', int, 0)
@@ -41,56 +43,64 @@ zmax = rdarg(argv, 'zmax', int, None)
 interpolate = rdarg(argv, 'interp', bool, True)
 
 if len(argv) < 3:
-	print("FitShow\n\
-	Print fits files in the terminal.\n\
-    Each pixel will be displayed as a number corresponding to its\n\
-    standard deviation with respect to the mean.\n\
-    Negative values will be displayed with '-'.\n\
-    Values above 10 sigma will be displayed with '*'.\n\
-    3d fits will be collapsed into two axis.\n\n\
-    Options:\n\
-            -name:  fits file name.\n\
-            -bin:   display numbers in multiples of the std (default=1).\n\
-            -dim:   dimension to be collapsed (0, 1 or 2, default=0).\n\
-            -{c}min:   minimum value of the dimension (c=x,y,z).\n\
-            -{c}max:   maximum value of the dimension (c=x,y,z).\n\
-            -std:   define your own standard deviation.\n\
-            -color: display numbers with colors (default=False).")
+	print("""FitShow\n\
+	Print fits files in the terminal.
+    Each pixel will be displayed as a number corresponding to its
+    standard deviation with respect to the mean.
+    Negative values will be displayed with '-'.
+    Values above 10 sigma will be displayed with '*'.
+    3d fits will be collapsed into two axis.
+    
+    Options:
+            -name:      fits file name.
+            -bin:       display numbers in multiples of the std (default=1).
+            -dim:       dimension to be collapsed (0, 1 or 2, default=0).
+            -{c}min:    minimum value of the dimension (c=x,y,z).
+            -{c}max:    maximum value of the dimension (c=x,y,z).
+            -std:       define your own standard deviation.
+            -color:     display numbers with colors (default=True).
+            -interp:    interpolate pixels to fit terminal size (default=True).""")
 
 if cube != '':
-	fit = getdata(cube)
+	fit = fits.getdata(cube)
 	shape = fit.shape
 	print('Initial image shape', shape)
-	if xmax is None: xmax = shape[2]
-	if ymax is None: ymax = shape[1]
-	if zmax is None: zmax = shape[0]
+	if xmax is None: xmax = shape[-1]
+	if ymax is None: ymax = shape[-2]
 	if len(shape) > 2:
+		print('Collapsing dimension', dim)
+		if zmax is None: zmax = shape[0]
 		fit = np.nanmean(fit[zmin:zmax + 1, ymin:ymax + 1, xmin:xmax + 1], dim)
+	else:
+		fit = fit[ymin:ymax + 1, xmin:xmax + 1]
 	yl, xl = fit.shape
 	if std is None: std = np.nanstd(fit)
-	print('Collapsing dimension', dim, '\nGenerating image with std', std, '\nNew image shape', fit.shape)
+	print('\nGenerating image with std', std, '\nNew image shape', fit.shape)
 	std = bin * std
-	## Interplation to terminal size
+	
+	## Interpolation to terminal size
 	if interpolate:
-        	from scipy.interpolate import interp2d
-        	char_aspect = 2  # Characters + space between them twice as high as wide
-        	rows, columns = os.popen('stty size', 'r').read().split()
-        	y, x = np.arange(yl), np.arange(xl)
-        	im_aspect = yl/xl
-        	# We want to fill the width of the term window, but allow scrolling
-        	newy, newx = np.linspace(0, y.max(), int(int(columns)*im_aspect/char_aspect)), np.linspace(0, x.max(), int(columns))#//char_aspect)
-        	fit = interp2d(x, y, fit, kind='linear')(newx, newy)
-        	yl, xl = fit.shape
+		from scipy.interpolate import interp2d
+		import os
+		char_aspect = 2  # Characters + space between them twice as high as wide
+		rows, columns = os.popen('stty size', 'r').read().split()
+		y, x = np.arange(yl), np.arange(xl)
+		im_aspect = yl / xl
+		# We want to fill the width of the term window, but allow scrolling
+		newy, newx = np.linspace(0, y.max(), int(int(columns) * im_aspect / char_aspect)), np.linspace(0, x.max(), int(
+			columns))  # //char_aspect)
+		fit = interp2d(x, y, fit, kind='linear')(newx, newy)
+		yl, xl = fit.shape
 	# End interpolation stuff
+	
 	s = ''
-	for y in range(yl):
+	for y in range(yl)[::-1]:
 		for x in range(xl):
 			d = fit[y, x]
 			if d < 0: s += '\033[0m' * colors + '-'
 			for i in range(10):
 				if (d >= i * std) & (d < (i + 1) * std):
 					s += ('\033[1;3%dm' % i) * colors + '%d' % i
-			if d >= 10 * std:
-				s += '\033[0m' * colors + '*'
+			if d >= 10 * std: s += '\033[0m' * colors + '*'
 		s += '\n'
 	print(s, '\033[0m ' * colors)
