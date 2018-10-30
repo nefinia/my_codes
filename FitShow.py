106e5fe59444a44893590c6e5308ebd8860238dc
Sofia G. Gallego <nefinia@gmail.com>
2018-10-30 19:50:01 +0100

    Update interpolation and reshape
    - Interpolation now fits the image completely within the terminal size
    - New argument "limits" defined as zmin,zmax,ymin,ymax,xmin,xmax

--- FitShow.py
+++ FitShow.py
@@ -23,9 +23,12 @@ def usage():
             -dim:       dimension to be collapsed (0, 1 or 2, default=0).
             -{c}min:    minimum value of the dimension (c=x,y,z).
             -{c}max:    maximum value of the dimension (c=x,y,z).
+            -limits:    limits for reshaping, format: (zmin,zmax,)ymin,ymax,xmin,xmax
             -std:       define your own standard deviation.
             -color:     display numbers with colors (default=True).
-            -interp:    interpolate pixels to fit terminal size (default=True)."""
+            -interp:    interpolate pixels to fit terminal size (default=True).
+            -aspect:    aspect ratio of the displayed image
+                        (default=2, preserving a squared pixel shape for standard terminals)"""
     print(usage.__doc__)
 
 
@@ -50,6 +53,7 @@ def rdarg(argv, key, type=None, default=None, listtype=int):
                 name = float(name)
             elif type is str:
                 name = str(name)
+            print(key, name)
             return name
     if default is not None: return default
 
@@ -67,65 +71,48 @@ ymin = rdarg(argv, 'ymin', int, 0)
 ymax = rdarg(argv, 'ymax', int, None)
 zmin = rdarg(argv, 'zmin', int, 0)
 zmax = rdarg(argv, 'zmax', int, None)
+limits = rdarg(argv, 'limits', list, None)
 interp = rdarg(argv, 'interp', bool, True)
+aspect = rdarg(argv, 'aspect', float, 2)
 if len(argv) < 3: usage()
 if '' == cube: exit(1)
 
-if len(argv) < 3:
-    print("""FitShow
-    Print fits files in the terminal.
-    Each pixel will be displayed as a number corresponding to its
-    standard deviation with respect to the mean.
-    Negative values will be displayed with '-'.
-    Values above 10 sigma will be displayed with '*'.
-    3d fits will be collapsed into two axis.
-
-    Options:
-            -name:      fits file name.
-            -bin:       display numbers in multiples of the std (default=1).
-            -dim:       dimension to be collapsed (0, 1 or 2, default=0).
-            -{c}min:    minimum value of the dimension (c=x,y,z).
-            -{c}max:    maximum value of the dimension (c=x,y,z).
-            -std:       define your own standard deviation.
-            -color:     display numbers with colors (default=True).
-            -interp:    interpolate pixels to fit terminal size (default=True).""")
-
-
-def reshape(fit, xmin, xmax, ymin, ymax, zmin, zmax):
+def reshape(fit, xmin, xmax, ymin, ymax, zmin, zmax, limits):
     """ Rescale on user demand : fit -> fit """
     shape = fit.shape
     print('Initial image shape', shape)
     if xmax is None: xmax = shape[-1]
     if ymax is None: ymax = shape[-2]
     if len(shape) > 2:
-        print('Collapsing dimension', dim)
         if zmax is None: zmax = shape[0]
+        if limits is not None: zmin, zmax, ymin, ymax, xmin, xmax = limits
+        print('Collapsing dimension', dim)
         fit = np.nanmean(fit[zmin:zmax + 1, ymin:ymax + 1, xmin:xmax + 1], dim)
     else:
+        if limits is not None: ymin, ymax, xmin, xmax = limits
         fit = fit[ymin:ymax + 1, xmin:xmax + 1]
+    print('Reshaped image', fit.shape)
     return fit
-
 
 def interpolate(fit):
     """ Interpolate to terminal size : fit -> fit"""
     from scipy.interpolate import interp2d
     import os
     yl, xl = fit.shape
-    char_aspect = 2 # Characters + space between them twice as high as wide
     rows, columns = os.popen('stty size', 'r').read().split()
+    size = min(int(columns), int(rows)*aspect)-4
     y, x = np.arange(yl), np.arange(xl)
-    im_aspect = yl / xl
+    im_ratio = yl/float(xl)
     # We want to fill the width of the term window, but allow scrolling
-    newy, newx = np.linspace(0, y.max(), int(int(columns) * im_aspect/char_aspect)), np.linspace(0, x.max(), int(
-        columns))
+    newy, newx = np.linspace(0, y.max(), int(size/aspect*im_ratio)), np.linspace(0, x.max(), size)
     # Log
-    print('Terminal size', len(newx), 'x', len(newy))
-    fit = interp2d(x, y, fit, kind='linear')(newy, newx)
-    print('New image shape', fit.shape)
+    print('Terminal size', columns, 'x', rows)
+    print('Image ratio',  im_ratio)
+    fit = interp2d(x, y, fit, kind='linear')(newx, newy)
+    print('Interpolated image shape', fit.shape)
     return fit
 
-
-def mesure_noise(fit):
+def measure_noise(fit):
     """ fit -> std """
     if std is None:
         noise = np.nanstd(fit)
@@ -134,41 +121,12 @@ def mesure_noise(fit):
     print('\nGenerating image with std', noise)
     return bin * noise
 
-
-def scale_log(i):
-    """ (float) i -> (int) i """
-    # Log for range 7-10 (3 intervales, 4 values)
-    if 6 < i:
-        i -= 6
-        i = log(i + 1)
-        i /= log(3)
-        i += 6
-    return i
-
-
-def print_cell(fit, x, y):
-    d = fit[y, x]
-    i = d / std
-    # Print '0' if negative
-    if 0 > i:
-        return '\033[0m' * colors + '-'
-    # Scale intensity
-    if logs: i = scale_log(i)
-    i = floor(i)
-    # Print its number
-    if 10 > i:
-        return ('\033[1;3%dm' % i) * colors + '%d' % i
-    # Print '*' if too high
-    return '\033[0m' * colors + '*'
-
-
 def print_fits(fit):
     """ Display (finally) the fit on term """
     yl, xl = fit.shape
     s = ''
     for y in range(yl)[::-1]:
         for x in range(xl):
-            s += print_cell(fit, x, y)
             d = fit[y, x]
             if d < 0: s += '\033[0m' * colors + '-'
             for i in range(10):
@@ -182,10 +140,10 @@ def print_fits(fit):
 fit = fits.getdata(cube)
 
 # Extract shape
-fit = reshape(fit, xmin, xmax, ymin, ymax, zmin, zmax)
+fit = reshape(fit, xmin, xmax, ymin, ymax, zmin, zmax, limits)
 
 # Noise calculation
-std = mesure_noise(fit)
+std = measure_noise(fit)
 
 # Interpolate (metrically on x and y)
 if interp: fit = interpolate(fit)
